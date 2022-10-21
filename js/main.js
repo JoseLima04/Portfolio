9a31ea511d751243d4e99f82055d3d9e99b69d8b
Jose Lima <jalimag0206@gmail.com>
2022-10-21 04:16:39 -0300

    create animateScroll.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import clickMenu from './modules/menu-mobile.js'
+import animeScroll from './modules/animateScroll.js';
 import typing from './modules/typing.js';
 
 clickMenu();
+animeScroll();
 typing();
---
+++ js/modules/animateScroll.js
@@ -0,0 +1,19 @@
+const item = document.querySelectorAll("[data-anime]")
+
+export default function animeScroll(){
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
+
+    item.forEach((element) => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add("animate");
+        } else {
+            element.classList.remove("animate");
+        }
+  });
+}
+
+animeScroll()
+
+window.addEventListener("scroll", () =>{
+    animeScroll()
+} )
