8241cd873e78569d0dc1bd9dc958be2d5917b980
Jose Lima <jalimag0206@gmail.com>
2022-10-25 13:46:08 -0300

    update

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+import clickMenu from '/assets/js/modules/menuMobile.js'
+import animeScroll from '/assets/js/modules/animateScroll.js';
+import typing from '/assets/js/modules/typing.js';
+
+clickMenu();
+animeScroll();
+typing();
