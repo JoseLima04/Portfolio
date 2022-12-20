ccaaee08b8563f7d08bb0d8202e8b354c93a4ff1
Jose Lima <jalimag0206@gmail.com>
2022-12-20 20:33:17 -0300

    iniciando card projetos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 import clickMenu from '/assets/js/modules/menuMobile.js'
 import animeScroll from '/assets/js/modules/animateScroll.js';
 import typing from '/assets/js/modules/typing.js';
-import verMais from '/assets/js/modules/verMais.js';
+//import verMais from '/assets/js/modules/verMais.js';
 import  *  as showSkills from '/assets/js/modules/showSkills.js';
 import hideSkill from './modules/hideSkill.js';
 
