25f2a95ddcd500ea34304fb41e5906fc88cd4fff
Jose Lima <jalimag0206@gmail.com>
2022-12-22 03:33:10 -0300

    botao ver mais finalizado

--- assets/js/modules/readMore.js
+++ assets/js/modules/readMore.js
@@ -2,6 +2,7 @@ const button = document.querySelector('.read-button');
 const button2 = document.querySelector('.read-button2');
 const button3 = document.querySelector('.read-button3');
 const button4 = document.querySelector('.read-button4');
+const readMore = document.querySelector('.verMais-projetos');
 
 export function lerMais() {
     const card = document.querySelector('.card');
@@ -47,7 +48,25 @@ export function lerMais4() {
     return button4.textContent = 'Ler Mais';
 }
 
+export function verMais() {
+    const card3 = document.querySelector('.card3');
+    const card4 = document.querySelector('.card4');
+
+    card3.classList.toggle('hidden')
+    card4.classList.toggle('hidden')
+    
+
+    if(card3.classList.contains('hidden') && card4.classList.contains('hidden')){
+        return readMore.textContent = 'Ver mais';
+    }
+
+    return readMore.textContent = 'Ver menos';
+}
+    
+
+
 button.addEventListener('click', lerMais);
 button2.addEventListener('click', lerMais2);
 button3.addEventListener('click', lerMais3);
 button4.addEventListener('click', lerMais4)
+readMore.addEventListener('click', verMais)
