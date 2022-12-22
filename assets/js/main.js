58d5b25a54cf6927984d792f9ed88fbd5ca40755
Jose Lima <jalimag0206@gmail.com>
2022-12-22 02:37:39 -0300

    card finalizados e botao ver mais criado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 import clickMenu from '/assets/js/modules/menuMobile.js'
 import animeScroll from '/assets/js/modules/animateScroll.js';
 import typing from '/assets/js/modules/typing.js';
-import lerMais from '/assets/js/modules/readMore.js';
+import * as readMore from '/assets/js/modules/readMore.js';
 import  *  as showSkills from '/assets/js/modules/showSkills.js';
 import hideSkill from './modules/hideSkill.js';
 
--- assets/js/modules/readMore.js
+++ assets/js/modules/readMore.js
@@ -1,6 +1,9 @@
 const button = document.querySelector('.read-button');
+const button2 = document.querySelector('.read-button2');
+const button3 = document.querySelector('.read-button3');
+const button4 = document.querySelector('.read-button4');
 
-export default function lerMais() {
+export function lerMais() {
     const card = document.querySelector('.card');
     card.classList.toggle('active');
 
@@ -11,4 +14,40 @@ export default function lerMais() {
     return button.textContent = 'Ler Mais';
 }
 
-button.addEventListener('click', lerMais)
+export function lerMais2() {
+    const card = document.querySelector('.card2');
+    card.classList.toggle('active');
+
+    if(card.classList.contains('active')){
+        return button2.textContent = 'Ler Menos';
+    }
+
+    return button2.textContent = 'Ler Mais';
+}
+
+export function lerMais3() {
+    const card = document.querySelector('.card3');
+    card.classList.toggle('active');
+
+    if(card.classList.contains('active')){
+        return button3.textContent = 'Ler Menos';
+    }
+
+    return button3.textContent = 'Ler Mais';
+}
+
+export function lerMais4() {
+    const card = document.querySelector('.card4');
+    card.classList.toggle('active');
+
+    if(card.classList.contains('active')){
+        return button4.textContent = 'Ler Menos';
+    }
+
+    return button4.textContent = 'Ler Mais';
+}
+
+button.addEventListener('click', lerMais);
+button2.addEventListener('click', lerMais2);
+button3.addEventListener('click', lerMais3);
+button4.addEventListener('click', lerMais4)
